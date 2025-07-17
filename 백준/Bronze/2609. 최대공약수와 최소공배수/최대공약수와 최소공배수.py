3af3728757714ba5794506b1a28a196fb1561d55
suwanbae <2019parasite@naver.com>
2025-07-17 22:57:54 +0900

    Revert "[Bronze I] Title: 최대공약수와 최소공배수, Time: 48 ms, Memory: 32412 KB -Baekjo…"

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++
@@ -1,9 +0,0 @@
-a,b = map(int,input().split())
-def gys(a,b):
-    if (a<b):
-        a,b = b,a 
-    while(b!=0):
-        a,b = b,a%b
-    return a
-result = gys(a,b)
-print(result, a*b//result, sep='\n')
